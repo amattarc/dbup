16555261c4b80a3cd61327c06a452f9201911ff9
Atchut Matta <atchut_b@hotmail.com>
2021-09-05 21:40:18 -0400

    initial commit

---
+++ DBUpConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using DbUp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace DBUpConsoleApp
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=(localdb)\\MSSQLLocalDB; Database=MyApp; Trusted_connection=true";
+
+            //var config = new ConfigurationBuilder()
+            //    //.SetBasePath()
+            //    .AddJson
+
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ DBUpConsoleApp/Scripts/08202021-CreateJobTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee](
+	[EmpID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[EmpName] [varchar](50) NULL,
+	[EmpLastName] [varchar](50) NULL,
+	[Dept] [varchar](10) NULL
+) ON [PRIMARY]
+GO
